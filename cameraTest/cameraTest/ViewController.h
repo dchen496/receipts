9a885bfea4252718f4400c061d12ea551f0ad2d2
Michael Chen <michael.chen@berkeley.edu>
2015-01-10 11:28:46 -0800

    add cameraTest

---
+++ cameraTest/cameraTest/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  cameraTest
+//
+//  Created by Michael Chen on 1/10/15.
+//  Copyright (c) 2015 Michael Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+
+@property BOOL newMedia;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)useCamera:(id)sender;
+- (IBAction)useCameraRoll:(id)sender;
+@property (strong, nonatomic) IBOutlet UIToolbar *toolBar;
+
+@end
+
---
+++ cameraTest/cameraTest/ViewController.m
@@ -0,0 +1,98 @@
+//
+//  ViewController.m
+//  cameraTest
+//
+//  Created by Michael Chen on 1/10/15.
+//  Copyright (c) 2015 Michael Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController (CameraDelegateMethods)
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)useCamera:(id)sender {
+        if ([UIImagePickerController isSourceTypeAvailable:
+             UIImagePickerControllerSourceTypeCamera])
+        {
+            UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+            imagePicker.delegate = self;
+            imagePicker.sourceType =
+            UIImagePickerControllerSourceTypeCamera;
+            imagePicker.mediaTypes = @[(NSString *) kUTTypeImage];
+            imagePicker.allowsEditing = NO;
+            [self presentViewController:imagePicker animated:YES completion:nil];
+            _newMedia = YES;
+        }
+}
+
+- (void)useCameraRoll:(id)sender
+    {
+        if ([UIImagePickerController isSourceTypeAvailable:
+             UIImagePickerControllerSourceTypeSavedPhotosAlbum])
+        {
+            UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+            imagePicker.delegate = self;
+            imagePicker.sourceType =
+            UIImagePickerControllerSourceTypePhotoLibrary;
+            imagePicker.mediaTypes = @[(NSString *) kUTTypeImage];
+            imagePicker.allowsEditing = NO;
+            [self presentViewController:imagePicker animated:YES completion:nil];
+            _newMedia = NO;
+        }
+}
+
+#pragma mark -
+#pragma mark UIImagePickerControllerDelegate
+
+-(void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSString *mediaType = info[UIImagePickerControllerMediaType];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
+        UIImage *image = info[UIImagePickerControllerOriginalImage];
+        
+        _imageView.image = image;
+        
+        if (_newMedia)
+            UIImageWriteToSavedPhotosAlbum(image,
+                                           self,
+                                           @selector(image:finishedSavingWithError:contextInfo:),
+                                           nil);
+    }
+}
+
+-(void)image:(UIImage *)image
+finishedSavingWithError:(NSError *)error
+ contextInfo:(void *)contextInfo
+{
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle: @"Save failed"
+                              message: @"Failed to save image"
+                              delegate: nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+
+
+@end

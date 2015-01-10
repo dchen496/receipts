d0ad71584719d5a52c4aa153bbfc2c446eb32f56
Archit Dua <archit@rfid4u.com>
2015-01-10 10:55:26 -0800

    Added second storyboard

---
+++ receipts/receipts/ReceiptDetails.h
@@ -0,0 +1,13 @@
+//
+//  ReceiptDetails.h
+//  receipts
+//
+//  Created by Archit Dua on 1/10/15.
+//  Copyright (c) 2015 Douglas Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceiptDetails : UIViewController
+
+@end

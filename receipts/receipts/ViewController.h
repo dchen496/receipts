df24086a63e9b16a75ef24728ff89f91ec765173
Douglas Chen <douglaspchen@gmail.com>
2015-01-10 01:49:31 -0800

    add xcode project

---
+++ receipts/receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  receipts
+//
+//  Created by Douglas Chen on 1/10/15.
+//  Copyright (c) 2015 Douglas Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

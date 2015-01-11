5822969c364181c91e605125c633a1967d8fc0e4
Douglas Chen <douglaspchen@gmail.com>
2015-01-10 16:58:22 -0800

    remove old receipts project

--- receipts/Pods/Target Support Files/Pods-receiptsTests-TesseractOCRiOS/Pods-receiptsTests-TesseractOCRiOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_receiptsTests_TesseractOCRiOS : NSObject
-@end
-@implementation PodsDummy_Pods_receiptsTests_TesseractOCRiOS
-@end
--- receipts/Pods/Target Support Files/Pods-receiptsTests/Pods-receiptsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_receiptsTests : NSObject
-@end
-@implementation PodsDummy_Pods_receiptsTests
-@end

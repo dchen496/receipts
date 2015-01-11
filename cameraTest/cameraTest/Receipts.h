bfa04d7136d4cfcdaebf1ce57affcd5661617a8f
Douglas Chen <douglaspchen@gmail.com>
2015-01-11 02:32:00 -0800

    aaaaaaaaaa

--- cameraTest/cameraTest/Receipts.h
+++ cameraTest/cameraTest/Receipts.h
@@ -9,7 +9,7 @@
 #ifndef cameraTest_Receipts_h
 #define cameraTest_Receipts_h
 
-extern NSMutableArray *receipt;
+extern NSMutableArray *receipt, *filteredReceipt;
 
 NSMutableArray *parseReceipt(NSString *input);
 double getPrice(NSArray *input, NSString *word);
@@ -17,5 +17,8 @@ NSMutableArray *removeExtraLines(NSArray *input);
 double getTotal(NSArray *input);
 double getUserTotal(NSArray *input, int user);
 double adjustForTaxes(double user, double total, double taxes);
+void assignUserToLine(NSMutableArray *input, int line, int user);
+void unassignLine(NSMutableArray *input, int line);
+int getLineUser(NSMutableArray *input, int line);
 
 #endif
